9b729130b2c691f56e4cee62008ff03c24fe65db
GauravSavaliya <gauravsavaliya718@gmail.com>
2021-05-03 18:06:28 +0530

    second commit

--- controllers/coursesController.js
+++ controllers/coursesController.js
@@ -1,5 +1,26 @@
 const Courses = require("../models/Courses");
 
-exports.getCourses = (req, res) => {
-  Courses.find({}).then((course) => res.json(course));
+exports.getCourses = async (req, res) => {
+  const courses = await Courses.find();
+
+  res.status(200).json({
+    success: true,
+    courses,
+  });
+};
+
+exports.getSingalCourse = async (req, res) => {
+  const course = await Courses.findById(req.params.id);
+
+  if (!course) {
+    return res.status(404).json({
+      success: false,
+      message: "Course not found!",
+    });
+  }
+
+  res.status(200).json({
+    success: true,
+    course,
+  });
 };
--- routes/courses.js
+++ routes/courses.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getCourses } = require("../controllers/coursesController");
+const {
+  getCourses,
+  getSingalCourse,
+} = require("../controllers/coursesController");
 
 router.route("/courses").get(getCourses);
+router.route("/course/:id").get(getSingalCourse);
 
 module.exports = router;
