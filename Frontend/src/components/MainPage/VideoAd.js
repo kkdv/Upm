b86b3bfe3b635b966663c49b3240ff6b75dd6a72
Krishna K <kkattumadam@gmail.com>
2021-07-19 00:10:14 -0700

    Videosize changes

--- Frontend/src/components/MainPage/VideoAd.js
+++ Frontend/src/components/MainPage/VideoAd.js
@@ -4,24 +4,17 @@ import "./VideoAd.css";
 function VideoAd(props) {
   return (
     <div className="videoAdDiv">
-      <div className="videoDiv">
+      <div className="videoDiv content">
         <iframe
           title={props.title}
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen=""
           src={props.docURL}
-          width="260"
-          height="215"
           frameBorder="0"
+          height="300"
+          width="300"
         ></iframe>
       </div>
-      {/* <div className="content">
-        <h2 className="heading">Transform your life through education</h2>
-        <p className="about">
-          Mohamad Alaloush launched a new career in software development by
-          taking courses on Udemy. What will you be able to do?
-        </p>
-      </div> */}
     </div>
   );
 }
